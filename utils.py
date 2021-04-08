0af2d6649f454dcafd6f8cf1080593ca4bbfd876
YOUR NAME <YOUR@EMAIL.com>
2021-04-08 20:39:49 +0700

    test

--- utils.py
+++ utils.py
@@ -1,21 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-import time
-
-PATH="chromedriver_linux64 (1)/chromedriver"
-
-options = Options()
-options.add_argument("headless")
-driver=webdriver.Chrome(PATH,options=options)
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.chrome.options import Options
+# import time
+#
+# PATH="chromedriver_linux64 (1)/chromedriver"
+#
+# options = Options()
+# options.add_argument("headless")
+# driver=webdriver.Chrome(PATH,options=options)
 
 
 
 ecomerce_webs=["https://www.amazon.com/","https://www.aliexpress.com/"]
 
-for website in ecomerce_webs:
-    driver.get(website)
-    print(driver.title)
+# for website in ecomerce_webs:
+#     driver.get(website)
 
 def amazon():
     # driver.get(ecomerce_webs[0])

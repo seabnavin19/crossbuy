87a19ac7caa2a67e7b337a7422f89f2064f30450
seab.navin19 <60089565+seabnavin19@users.noreply.github.com>
2021-04-08 20:17:22 +0700

    Add files via upload

---
+++ flask_server.py
@@ -0,0 +1,13 @@
+from flask import Flask,redirect,url_for,render_template
+# import utils
+app=Flask(__name__)
+
+
+@app.route("/")
+def homePage():
+    my="mr"
+    return  render_template("index.html",me=my)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
